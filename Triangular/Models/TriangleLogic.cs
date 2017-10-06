8c5bf171f7d397e1b9b1ceaad0fb51d4ff921077
David Stiennon <david.stiennon@gmail.com>
2017-10-06 12:54:06 -0500

    Getting started

---
+++ Triangular/Models/TriangleLogic.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangular.Models
+{
+    public class TriangleLogic
+    {
+        public string Classify(List<decimal> sides)
+        {
+            if (IsNotATriangle(sides))
+            {
+                return "Not a Triangle";
+            }
+            if (IsEquilateral(sides))
+            {
+                return "Equilateral";
+            }
+            if (IsIsosceles(sides))
+            {
+                return "Isosceles";
+            }
+            return ScaleneClass(sides);
+        }
+
+        public string ScaleneClass(List<decimal> sides)
+        {
+            var maxSide = sides.Max();
+            var othersSides = sides.OrderByDescending(x => x).Skip(1);
+            var aSquaredPlusBSquare = othersSides.Sum(x => x * x);
+            var cSquared = maxSide*maxSide;
+            if (aSquaredPlusBSquare == cSquared)
+            {
+                return "Scalene: Right";
+            }
+            if (aSquaredPlusBSquare < cSquared)
+            {
+                return "Scalene: Obtuse";
+            }
+            return "Scalne: Acute";
+        }
+
+        public bool IsIsosceles(List<decimal> sides)
+        {
+            return sides.Distinct().Count() < 3;
+        }
+
+        public bool IsEquilateral(List<decimal> sides)
+        {
+            return sides.Distinct().Count() < 2;
+        }
+
+        public bool IsNotATriangle(List<decimal> sides)
+        {
+            var maxSide = sides.Max();
+            var othersSides = sides.OrderByDescending(x => x).Skip(1);
+            return sides.Count() != 3
+                   || sides.Any(s => s <= 0)
+                   || maxSide > othersSides.Sum();
+        }
+    }
+}
---
+++ Triangular/Scripts/src/app.js
@@ -0,0 +1,19 @@
+﻿
+const TriangleCtrl = function($http) {
+    this.triangles = [
+        1, 2, 3
+    ];
+}
+
+TriangleCtrl.prototype.addTriangle = function() {
+    this.triangles.push({
+        sides: [0, 0, 0],
+        type: ''
+    });
+}
+
+TriangleCtrl.prototype.removeTriangle = function(index) {
+    this.triangles.splice(index, 1);
+}
+
+angular.module('triangular', []).controller('TriangleCtrl', ['$http', TriangleCtrl ]);
